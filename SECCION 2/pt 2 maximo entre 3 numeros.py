663d0833dca08d8a171254f3becfc95c743456c3
lissa cardozo martinez <mariaf.sanchezca@ecci.edu.co>
2024-05-24 10:58:55 -0500

    agregue pt 2 en seccion 2

---
+++ SECCION 2/pt 2 maximo entre 3 numeros.py
@@ -0,0 +1,14 @@
+# seccion: 2
+#tema: condicionales
+#punto 2
+#Escriba un programa para encontrar el máximo entre tres números.
+x=float(input("ingrese el número x"))
+y=float(input("ingrese el número y"))
+z=float(input("ingrese el número z"))
+if x > y and z:
+         print("x es mayor")
+if y > x and z:
+         print("y es mayor")
+if z > x and y:
+         print("z es mayor")
+        
