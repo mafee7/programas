d43ef0377771e9ede635e4cedbb69f2241d9b984
mafee7 <mariaf.sanchezca@ecci.edu.co>
2024-05-28 23:43:38 -0500

    Add files via upload

--- SECCION 3/pt 4 mediana.py
+++ SECCION 3/pt 4 mediana.py
@@ -4,10 +4,4 @@
 
 #dada una llista numeros hallar la mediana
 
-lista_numeros = [5, 3, 8, 1, 9, 7, 4]
-lista_ordenada = sorted(lista_numeros)
 
-n = len(lista_ordenada)
-
-mediana = lista_ordenada[n // 2]
-print("La mediana es:", mediana)
---
+++ SECCION 3/pt 6 varianza.py
@@ -0,0 +1,5 @@
+#seccion:3
+#tema: listas
+#punto 6
+
+#dada una lista de numeros hallar la varianza
