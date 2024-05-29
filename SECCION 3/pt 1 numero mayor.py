9455897f4bafa84861768cbc3ffb015a80d2bfa7
mafee7 <mariaf.sanchezca@ecci.edu.co>
2024-05-29 11:22:54 -0500

    pt 1 numero mayor.py

--- SECCION 3/pt 1 numero mayor.py
+++ SECCION 3/pt 1 numero mayor.py
@@ -3,10 +3,10 @@
 #punto 1
 lista1=[2,-5,8,3,9]
 mayor=-100
-for i in range(len(lista2)):
+for i in range(len(lista1)):
     
     if lista1[i]>mayor:
-        mayor=lista2[i]
+        mayor=lista1[i]
  
 print("el numero mayor es",mayor)
 
