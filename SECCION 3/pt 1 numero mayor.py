566f191cd16da660477176a8a30e2494903002ce
mafee7 <mariaf.sanchezca@ecci.edu.co>
2024-05-29 11:16:44 -0500

    pt 1 numero mayor.py

--- SECCION 3/pt 1 numero mayor.py
+++ SECCION 3/pt 1 numero mayor.py
@@ -1,15 +1,12 @@
 # seccion: 3
 # tema: listas
 #punto 1
-lista1=[2,-5,8,3,9]
-lista2=[3,2,1]
-lista_nombres=["mafe","andres","ernesto","juan","nico"]
-lista1[2]
+lista2=[2,-5,8,3,9]
+mayor=-100
+for i in range(len(lista2)):
+    
+    if lista2[i]>mayor:
+        mayor=lista2[i]
+ 
+print("el numero mayor es",mayor)
 
-numero_mayor= -10000
-for elemento in lista1:
-    print(elemento)
-    #comparar cada elemento con el numero_mayor
-    if elemeto>numero_mayor:
-        numero_mayor= elemento
-print("el numero mayor es:", numero_mayor)
